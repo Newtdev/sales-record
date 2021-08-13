be158e04a6f5970585756c49e156e70472404cec
Thomas <ejembithomas61@gmail.com>
2021-08-13 11:56:56 +0100

    remove google auth

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -1,10 +1,9 @@
 import axios from "axois";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export default axios.create({
-  //   baseURL: "http://localhost:8080",
-  AnonKey: supabaseAnonKey,
-  AppURL: supabaseUrl
+  baseURL:
+    "https://irndfzxfhqdxkbrliucy.supabase.co/rest/v1/sales?select=userid",
+  AnonKey: supabaseAnonKey
 });
