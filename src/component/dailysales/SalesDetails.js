c33ff6df7ad9454d587b4b16a35a6b2db4d67220
Thomas <ejembithomas61@gmail.com>
2021-08-26 02:48:01 +0100

    daily sales code commented out

--- src/component/dailysales/SalesDetails.js
+++ src/component/dailysales/SalesDetails.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 const SalesDetails = props => {
+  const records = [
+    { id: 1, quantity: 2000, cost: 3000, sell: 3500, date: new Date().now() },
+    { id: 2, quantity: 1000, cost: 2000, sell: 3500, date: new Date().now() },
+    { id: 3, quantity: 4000, cost: 4500, sell: 5500, date: new Date().now() }
+  ];
+
+  const dailyRecords = records.map(data => {
+    return <div></div>;
+  });
   return <div></div>;
 };
 
